bc99652973aca58ef8c6768e62bd9cd18d43cdb4
Chris Pryer <cnpryer@gmail.com>
2023-09-22 21:15:23 -0400

    Use 4 spaces when formatting

--- src/index.js
+++ src/index.js
@@ -2,42 +2,41 @@
  * Ripe: An LRUCache for JavaScript.
  */
 
-
 class LRUCache {
-	constructor(capacity) {
-		this.capacity = capacity;
-		this.cache = new Map();
-	}
+    constructor(capacity) {
+        this.capacity = capacity;
+        this.cache = new Map();
+    }
 
-	get(key) {
-		if (this.cache.has(key)) {
-			const value = this.cache.get(key);
-			this.cache.delete(key);
-			this.cache.set(key, value);
-			return value;
-		}
-		return -1;
-	}
+    get(key) {
+        if (this.cache.has(key)) {
+            const value = this.cache.get(key);
+            this.cache.delete(key);
+            this.cache.set(key, value);
+            return value;
+        }
+        return -1;
+    }
 
-	put(key, value) {
-		if (this.cache.has(key)) {
-			this.cache.delete(key);
-		} else if (this.cache.size >= this.capacity) {
-			const keys = this.cache.keys();
-			const firstKey = keys.next().value;
-			this.cache.delete(firstKey);
-		}
+    put(key, value) {
+        if (this.cache.has(key)) {
+            this.cache.delete(key);
+        } else if (this.cache.size >= this.capacity) {
+            const keys = this.cache.keys();
+            const firstKey = keys.next().value;
+            this.cache.delete(firstKey);
+        }
 
-		this.cache.set(key, value);
-	}
+        this.cache.set(key, value);
+    }
 
-	size() {
-		return this.capacity;
-	}
+    size() {
+        return this.capacity;
+    }
 
-	clear() {
-		this.cache.clear();
-	}
+    clear() {
+        this.cache.clear();
+    }
 }
 
 module.exports = LRUCache;
--- test/cache.test.js
+++ test/cache.test.js
@@ -1,43 +1,43 @@
 const LRUCache = require("../src/index");
 
 test("cache put", () => {
-	const cache = new LRUCache(3);
-	cache.put(1, 1);
-	expect(cache.get(1)).toBe(1);
-	expect(cache.get(0)).toBe(-1);
+    const cache = new LRUCache(3);
+    cache.put(1, 1);
+    expect(cache.get(1)).toBe(1);
+    expect(cache.get(0)).toBe(-1);
 });
 
 test("cache get", () => {
-	const cache = new LRUCache(3);
-	cache.put(1, 1);
-	expect(cache.get(1)).toBe(1);
-	expect(cache.get(0)).toBe(-1);
+    const cache = new LRUCache(3);
+    cache.put(1, 1);
+    expect(cache.get(1)).toBe(1);
+    expect(cache.get(0)).toBe(-1);
 });
 
 test("cache size", () => {
-	const cache = new LRUCache(3);
-	expect(cache.size()).toBe(3);
+    const cache = new LRUCache(3);
+    expect(cache.size()).toBe(3);
 });
 
 test("cache clear", () => {
-	const cache = new LRUCache(3);
-	cache.put(1, 1);
-	cache.put(2, 2);
-	cache.put(3, 3);
-	cache.clear();
-	expect(cache.get(1)).toBe(-1);
-	expect(cache.get(2)).toBe(-1);
-	expect(cache.get(3)).toBe(-1);
+    const cache = new LRUCache(3);
+    cache.put(1, 1);
+    cache.put(2, 2);
+    cache.put(3, 3);
+    cache.clear();
+    expect(cache.get(1)).toBe(-1);
+    expect(cache.get(2)).toBe(-1);
+    expect(cache.get(3)).toBe(-1);
 });
 
 test("cache capacity", () => {
-	const cache = new LRUCache(3);
-	cache.put(1, 1);
-	cache.put(2, 2);
-	cache.put(3, 3);
-	expect(cache.get(1)).toBe(1);
-	expect(cache.get(2)).toBe(2);
-	expect(cache.get(3)).toBe(3);
-	cache.put(4, 4);
-	expect(cache.get(1)).toBe(-1);
+    const cache = new LRUCache(3);
+    cache.put(1, 1);
+    cache.put(2, 2);
+    cache.put(3, 3);
+    expect(cache.get(1)).toBe(1);
+    expect(cache.get(2)).toBe(2);
+    expect(cache.get(3)).toBe(3);
+    cache.put(4, 4);
+    expect(cache.get(1)).toBe(-1);
 });
