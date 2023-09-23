c7fbe13b6f23587de2ed7eb650e92521d279f132
Chris Pryer <cnpryer@gmail.com>
2023-09-23 08:56:01 -0400

    Resolve TypeScript lint

--- src/index.ts
+++ src/index.ts
@@ -14,8 +14,14 @@ class LRUCache<K, V> {
     get(key: K): V | -1 {
         if (this.cache.has(key)) {
             const value = this.cache.get(key);
+
+            if (value === undefined) {
+                return -1;
+            }
+
             this.cache.delete(key);
             this.cache.set(key, value);
+
             return value;
         }
 
@@ -26,9 +32,10 @@ class LRUCache<K, V> {
         if (this.cache.has(key)) {
             this.cache.delete(key);
         } else if (this.cache.size >= this.capacity) {
-            const firstKey = this.cache.keys().next().value;
-            if (firstKey !== undefined) {
-                this.cache.delete(firstKey);
+            const key = this.cache.keys().next().value;
+
+            if (key !== undefined) {
+                this.cache.delete(key);
             }
         }
 
